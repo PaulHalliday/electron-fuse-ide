8a0a2336f74c93349513a93f6fa6d4a7379a815e
Kristian Mandrup <kmandrup@gmail.com>
2016-07-22 19:33:37 +0200

    canvas menu

---
+++ app/menus/canvas/index.js
@@ -0,0 +1,22 @@
+class Grid {
+  
+}
+
+class Rulers {
+
+}
+
+class Size {
+
+}
+
+class Zoom {
+
+}
+
+export default {
+  grid: Grid,
+  rulers: Rulers,
+  size: Size,
+  zoom: Zoom
+}
--- app/menus/view/index.js
+++ app/menus/view/index.js
@@ -1,7 +1,15 @@
 import ClipBoardActions from './ClipBoardActions';
+import Misc from './Misc';
+import SetColor from './SetColor';
+
+// turn a widget into a Template
+class Template {
+
+}
 
 export default {
   ClipBoardActions: ClipBoardActions,
   Misc: Misc,
   SetColor: SetColor
+  Template: Template,
 }
